2b0fcb34ad1b5bef7a58b1e8c4504d981f300dee
OlofHarrysson <harrysson.olof@gmail.com>
2017-04-21 15:23:36 +0200

    created graph between actors

---
+++ main.py
@@ -0,0 +1,62 @@
+import numpy as np
+
+
+def read_file(path): # Splits at blank space
+    file = open(path, 'r')
+    return file.read().split('\n')
+
+class Actor():
+    def __init__(self, name):
+        self.name = name
+        self.edges = dict()
+
+    def set_edge(self, actor):
+        self.edges[actor.name] = actor
+
+    def __str__(self):  # TODO edges
+        return "Name is {:s}. Nbr of edges is {:d}".format(self.name, len(self.edges))
+
+def create_edges(actor_names, actors):
+    actors_in_movie = [actors[name] for name in actor_names]
+
+    for i in range(len(actors_in_movie)):
+        for j in range(i+1, len(actors_in_movie)):
+            a1 = actors_in_movie[i]
+            a2 = actors_in_movie[j]
+
+            a1.set_edge(a2)
+            a2.set_edge(a1)
+
+def create_graph():
+    casts = read_file('casts.csv')
+
+    movies_dict = dict()
+    actors_temp = []
+    for line in casts:
+        line = line.split(';')
+        actor = line[2].replace('"', '')
+        movie = line[1].replace('"', '')
+        actors_temp.append(actor)
+
+        try:
+            movies_dict[movie].append(actor)
+        except KeyError:
+            movies_dict[movie] = [actor]
+
+
+    actors_temp = set(actors_temp)
+
+    actors = dict()
+    for actor in actors_temp:
+        actors[actor] = Actor(actor)
+
+    for movie, actor_names in movies_dict.items():
+        create_edges(actor_names, actors)
+
+#################################### START ####################################
+
+create_graph()
+
+
+
+# print(movies_dict)
